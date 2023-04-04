eb9eb88f0dbd53c25a305a19612ac627e4397e15
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-04-04 15:48:33 +0600

    Added a React Router Dom package

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-
-import './App.css'
-
-function App() {
-
+export default function App(){
   return (
     <div className="App">
    <h1>Hello world</h1>
     </div>
-  )
+  );
 }
 
-export default App
