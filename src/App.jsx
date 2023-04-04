003a2753aa78a8ff19d7f507df26c6bbbee12ae7
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-04-04 16:03:29 +0600

    Added the navigation to the ap components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-export default function App(){
+import { NavLink } from "react-router-dom";
+
+export default function App() {
   return (
     <div className="App">
-   <h1>Hello world</h1>
+      <h1>Hello World</h1>
+      <p>This is my diploma</p>
+      <nav>
+        <NavLink>Home</NavLink>
+        <NavLink>About</NavLink>
+      </nav>
     </div>
   );
 }
-
