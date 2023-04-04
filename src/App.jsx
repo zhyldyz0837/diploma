40130426b528aa8eec5907cb83166e091ecdd245
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-04-04 16:21:08 +0600

    add basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
@@ -6,9 +6,17 @@ export default function App() {
       <h1>Hello World</h1>
       <p>This is my diploma</p>
       <nav>
-        <NavLink>Home</NavLink>
-        <NavLink>About</NavLink>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
       </nav>
+      <main>
+        <Routes>
+          <Route path="/" element={<h2>Home</h2>} />
+          <Route path="/about" element={<h2>About</h2>} />
+          <Route path="/contact" element={<h2>Contact</h2>} />
+        </Routes>
+      </main>
     </div>
   );
 }
