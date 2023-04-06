f9613ad6572aba8043201a4f37a4dc49b6e0b116
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-04-06 15:06:28 +0600

    Added to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout/Layout"; 
+import Layout from "./components/Layout/Layout";
 
 export default function App() {
   return (
@@ -9,6 +9,7 @@ export default function App() {
           <Route path="/" element={<h2>Home</h2>} />
           <Route path="/about" element={<h2>About</h2>} />
           <Route path="/contact" element={<h2>Contact</h2>} />
+          <Route path="/delivery" element={<h2>Delivery</h2>} />
         </Routes>
       </Layout>
     </div>
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,12 +1,13 @@
 import "./Nav.css";
 import { NavLink } from "react-router-dom";
 
-export default function Nav(){
+export default function Nav() {
   return (
     <nav classNAme="Nav">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/contact">Contact</NavLink>
+      <NavLink to="/delivery">Delivery</NavLink>
     </nav>
-  )
+  );
 }
