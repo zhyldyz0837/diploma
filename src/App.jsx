e41768cbb2c4a4cc7c361ba5ba4e0c66e68da099
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-04-11 16:19:48 +0600

    imported category

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,18 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Delivery from "./pages/Delivery";
-
+import Category from "./pages/Categories";
 
 export default function App() {
   return (
     <div className="App">
       <Layout>
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/contact" element={<Contact/>} />
-          <Route path="/delivery" element={<Delivery></Delivery>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/delivery" element={<Delivery />} />
+          <Route path="/category/:path" element={<Category/>}/>
         </Routes>
       </Layout>
     </div>
