061f27a8003da677327cd90b399633f20b6a7b35
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-04-11 16:17:50 +0600

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCuJqWQUSgGlVCvogWbBZG9HfbcWRXVx4A",
+  authDomain: "diploma-38ac5.firebaseapp.com",
+  projectId: "diploma-38ac5",
+  storageBucket: "diploma-38ac5.appspot.com",
+  messagingSenderId: "87978906890",
+  appId: "1:87978906890:web:4f8c6660868ab6a3a596f3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export const categoryCollection  = collection(db, 'categories');
