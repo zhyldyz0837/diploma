6f8cb9f965de4cabb6d608196bbb16705746940d
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-04-12 22:20:36 +0600

    added paragraph to about.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,9 @@
 export default function  About(){
   return (
     <div className=" About">
-    <h1>Welcome to my Candy shop </h1>
-  <p>This is my awesome Candy shop </p>
-  <p>We only use natural ingredients to make our yummy candy shop </p>
+      <h1>Welcome to my Candy shop </h1>
+      <p>This is my awesome Candy shop</p>
+      <p>We only use natural ingredients to make our yummy candy shop </p>
     </div>
   )
 }
