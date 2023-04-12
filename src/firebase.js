fab63a1b8a1861a6029c593dd1ae53bd7de12110
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-04-12 22:07:13 +0600

    exported CategoryList

--- src/firebase.js
+++ src/firebase.js
@@ -17,4 +17,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-export const categoryCollection  = collection(db, 'categories');
+export default collection(db, 'categories');
