30adbb925e46d747851ca1c36f85c935015a37ce
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-04-13 10:31:44 +0600

    styled links in Nav  css

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
 import Delivery from "./pages/Delivery";
-import Category from "./pages/Categories";
+import Contact from "./pages/Contact";
 
 export default function App() {
   return (
@@ -13,6 +12,8 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/delivery" element={<Delivery />} />
         </Routes>
       </Layout>
     </div>
