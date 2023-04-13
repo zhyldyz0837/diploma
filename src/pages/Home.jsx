60a96771b205fe45f8431f599a5860a2bb3b1bf6
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-04-13 07:42:29 +0600

    added Home new paragraph and header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,7 @@
-export default function Home() {
+export default function Category() {
   return (
-    <div className="Home">
-      <h1>Welcome to my Candy shop </h1>
-      <p>This is my awesome Candy shop</p>
-      <p>We only use natural ingredients to make our yummy candy shop </p>
+    <div className="Category">
+      <h1>Category</h1>
     </div>
   );
 }
