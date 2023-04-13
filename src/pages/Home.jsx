5d62a7d4c6c3c1385be0f35db16150dcf394e389
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-04-13 11:03:00 +0600

    Changed  paragraph

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 export default function Category() {
   return (
     <div className="Category">
-      <h1>Category</h1>
+      <h1>Welcome to my Candy shop </h1>
+      <p>This is my awesome Candy shop</p>
+      <p>We only use natural ingredients to make our yummy candy shop </p>
     </div>
   );
 }
