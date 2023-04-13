32a84e2771c9c890a9e82a3f7f89eb1f8e260ecd
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-04-13 07:45:28 +0600

    Imported firebase

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,42 +1,42 @@
-import { getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import categoryCollection from "../../firebase";
 import "./CategoryList.css";
+import { getDocs } from "firebase/firestore";
+import { categoryCollection } from "../../firebase";
 import { NavLink } from "react-router-dom";
 
-export default function CategoryList () {
+export default function CategoryList() {
   const [categories, setCategories] = useState([]);
 
-  useEffect(() =>{
-    getDocs(categoryCollection)
-    .then(snapshot => {
-      const newCategories =[];
+  // выполнить эту функцию только один раз
+  useEffect(() => {
+    // получить категории из списка категорий
+    getDocs(categoryCollection).then((snapshot) => {
+      // категории будут храниться в snapshot.docs
 
-       snapshot.docs.forEach(doc => {
+      // создать массив для категорий
+      const newCategories = [];
+      // заполнить массив данными из списка категорий
+      snapshot.docs.forEach((doc) => {
+        // doc = категория
         const category = doc.data();
         category.id = doc.id;
 
         newCategories.push(category);
-       });
+      });
+      // задать новый массив как состояние комапо
+      setCategories(newCategories);
+    });
+  }, []);
 
-       setCategories(newCategories);
-    })
- 
-  },[]);
-  
-  const output = categories.map(category => (
+  const output = categories.map((category) => (
     <li key={category.id}>
-      <NavLink to={'/category' + category.path}>
-        {category.name}
-      </NavLink>
+      <NavLink to={"/category/" + category.path}>{category.name}</NavLink>
     </li>
   ));
 
   return (
     <div className="CategoryList">
-      <ul>
-        {output}
-      </ul>
+      <ul>{output}</ul>
     </div>
   );
 }
