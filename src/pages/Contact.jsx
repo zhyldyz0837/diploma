adc8f6b8d545138240dbaecf8ae0c8b877c22825
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-04-13 10:33:00 +0600

    added paragraph  to Contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,8 @@ export default function Contact() {
   return (
     <div className="Contact">
       <h1>Welcome to my Candy shop </h1>
-      <p>This is my awesome Candy shop </p>
+      <p>This is my awesome Candy shop</p>
+      <p>We only use natural ingredients to make our yummy candy shop </p>
     </div>
   );
 }
