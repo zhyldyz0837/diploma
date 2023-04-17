ed000d4eb4cc461841df873a3307a5d4bc9714b2
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-04-17 17:22:33 +0600

    Addproducts to ProductList component

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,9 +1,26 @@
-import ".ProductList.css";
+import "./ProductList.css";
+import { useContext } from "react";
+import { AppContext } from "../../App";
+import { Link } from "react-router-dom";
 
 export default function ProductList() {
+const { products } = useContext(AppContext);
+
+const output = products.map(product => (
+  <div className="Product" key={product.id}>
+  <img src={product.picture} alt={product.name}/>
+  <Link to={"/product/" + product.path}>
+    {product.name}
+  </Link>
+  <span>{product.price} som</span>
+  </div>
+));
+
+
+
   return (
      <div className="ProductList">
-
+      {output}
     </div>
     );
   }
