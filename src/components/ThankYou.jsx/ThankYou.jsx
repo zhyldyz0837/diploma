d323117671805d0176e763a0fbb7fad146028e0d
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-04-20 16:33:30 +0600

    added and imported  GoogleAuthProvider,   getAuth, onAuthStateChanged,   signInWithPopup, from "firebase/auth"

---
+++ src/components/ThankYou.jsx/ThankYou.jsx
@@ -0,0 +1,8 @@
+export default function ThankYou() {
+  return (
+    <div className="ThankYou">
+      <h1>Thank you!</h1>
+      <p>Your order has been created. Please wait for a confirmation call.</p>
+    </div>
+  );
+}
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { collection, getFirestore } from "firebase/firestore";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  onAuthStateChanged,
+  signInWithPopup,
+} from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,7 +21,14 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
 // получить список категорий (коллекция документов).
 export const categoryCollection = collection(db, "categories");
 export const productCollection = collection(db, "products");
+export const orderCollection = collection(db, "orders");
+
+const provider = new GoogleAuthProvider();
+export const logIn = () => signInWithPopup(auth, provider);
+export const logOut = () => signInWithPopup(auth);
+export const onAuthChange = (callback) => onAuthStateChanged(auth, callback);
