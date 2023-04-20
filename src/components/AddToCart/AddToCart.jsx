35acbcb0680c24a6dcc12317ed7c298e21723eca
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-04-20 13:32:46 +0600

    Add to cart to AddToCart.jsx

---
+++ src/components/AddToCart/AddToCart.jsx
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import "./AddToCart.css";
+import { AppContext } from "../../App";
+
+export default function AddToCart({ product }) {
+  const { cart, setCart } = useContext(AppContext);
+
+  const currentCount = cart[product.id] ? cart[product.id] : 0;
+  function onAddToCart() {
+    setCart({
+      ...cart,
+      [product.id]: currentCount + 1,
+    });
+  }
+
+  return (
+    <div className="AddToCart">
+      <button onClick={onAddToCart}>Add to cart ({currentCount})</button>
+    </div>
+  );
+}
