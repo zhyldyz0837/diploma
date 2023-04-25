1881c547ad47c67f25a0b1fbb18396a6ef557920
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-04-25 13:55:49 +0600

    Changed photo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,12 +1,12 @@
 import "./Logo.css";
-import logo from "../../assets/logo.svg";
+import logo from "../../assets/logo.jpg";
 import { NavLink } from "react-router-dom";
 
 export default function Logo() {
   return (
     <div className="Logo">
       <NavLink to="/">
-        <img src={logo} alt="Website logo" />
+        <img id="navlink" src={logo} alt="Website logo" />
       </NavLink>
     </div>
   );
