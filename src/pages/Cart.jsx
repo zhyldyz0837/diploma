7dfa57f9a901d391d2a2cec625380162ac182b99
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-04-25 13:59:16 +0600

    imported OrderForm

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,12 @@
 import CartList from "../components/CartList/CartList";
+import OrderForm from "../components/OrderForm/OrderForm";
 
 export default function Cart() {
   return (
     <div className="Cart">
       <h1>Cart</h1>
       <CartList />
+      <OrderForm />
     </div>
   );
 }
