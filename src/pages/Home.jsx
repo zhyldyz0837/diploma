7bb4fb0f831fc22855e499fb116b5d14bd631ffd
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-05-11 14:04:54 +0600

    Added image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Category() {
   return (
     <div className="Category">
-      <img src="https://i.pinimg.com/564x/a0/3c/41/a03c414700c7dc7b61bcba5ca0cfccd5.jpg" />
+      <img src="https://i.pinimg.com/564x/94/28/7d/94287d21086aa8e9d0694d34e7ee1916.jpg" />
       <h1>Welcome to my Candy shop </h1>
       <p>This is my awesome Candy shop</p>
       <p>We only use natural ingredients to make our yummy candy shop </p>
