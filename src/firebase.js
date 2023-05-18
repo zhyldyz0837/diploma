1e747f0a23bb6fd842f0213355adda7d09c93839
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-05-18 09:16:06 +0600

    we did adding pictures was only once

--- src/firebase.js
+++ src/firebase.js
@@ -64,11 +64,14 @@ export const onOrdersLoad = (callback) =>
     )
   );
 
-  // отправка фотографии и получение ее url
-export const uploadProductPhoto = async (file) => {
+// отправка фотографии и получение ее url
+export const uploadProductPhoto = (file) => {
   const storageRef = ref(storage, `products/${file.name}`);
-  await uploadBytes(storageRef, file);
-
-  const url = await getDownloadURL(storageRef);
-  return url;
+  return uploadBytes(storageRef, file)
+    .then(() => {
+      return getDownloadURL(storageRef);
+    })
+    .catch((error) => {
+      console.log("Failed to upload product photo:", error);
+    });
 };
