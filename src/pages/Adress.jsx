b9917f4dff43dca5e32c83edc3fd689fa5192e4b
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-05-19 14:37:00 +0600

    Deleted Adress

--- src/pages/Adress.jsx
+++
@@ -1,9 +0,0 @@
-export default function Adress() {
-  return (
-    <div className=" Adress">
-      <h1>Welcome to my Candy shop </h1>
-      <p>This is my awesome Candy shop</p>
-      <p>We only use natural ingredients to make our yummy candy shop </p>
-    </div>
-  );
-}
