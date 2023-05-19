b6359b808df3ef6e88cad1295eb03e26784458aa
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-05-19 08:37:32 +0600

    Changed Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,3 @@
-// export default function Home() {
-//   return (
-//     <div className="Home">
-//       <div className="home-img">
-//         {" "}
-//         <img src="https://i.pinimg.com/564x/94/28/7d/94287d21086aa8e9d0694d34e7ee1916.jpg" />
-//       </div>
-//       <div className="home-text">
-//         {" "}
-//         <h1>Welcome to my Candy shop </h1>
-//         <p>This is my awesome Candy shop</p>
-//         <p>We only use natural ingredients to make our yummy candy shop </p>
-//       </div>
-//     </div>
-//   );
-// }
-
-// // https://i.pinimg.com/564x/89/78/80/897880bd2c56e94018f0a319c9365087.jpg
-
 export default function Home() {
   return (
     <div className="Home">
