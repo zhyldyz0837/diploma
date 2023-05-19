b43d42762293c436591a441e34bb35888cc582ea
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-05-19 08:35:34 +0600

    added a picture and on hover made it  transition

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,37 @@
+// export default function Home() {
+//   return (
+//     <div className="Home">
+//       <div className="home-img">
+//         {" "}
+//         <img src="https://i.pinimg.com/564x/94/28/7d/94287d21086aa8e9d0694d34e7ee1916.jpg" />
+//       </div>
+//       <div className="home-text">
+//         {" "}
+//         <h1>Welcome to my Candy shop </h1>
+//         <p>This is my awesome Candy shop</p>
+//         <p>We only use natural ingredients to make our yummy candy shop </p>
+//       </div>
+//     </div>
+//   );
+// }
+
+// // https://i.pinimg.com/564x/89/78/80/897880bd2c56e94018f0a319c9365087.jpg
+
 export default function Home() {
   return (
     <div className="Home">
-      <div className="home-img">
-        {" "}
-        <img src="https://i.pinimg.com/564x/94/28/7d/94287d21086aa8e9d0694d34e7ee1916.jpg" />
-      </div>
-      <div className="home-text">
-        {" "}
-        <h1>Welcome to my Candy shop </h1>
-        <p>This is my awesome Candy shop</p>
-        <p>We only use natural ingredients to make our yummy candy shop </p>
+      <div class="home-header">
+        <div class="home-header-img">
+          <img
+            src="https://i.pinimg.com/564x/31/bb/15/31bb15f744904d29835a99e4946f51c1.jpg"
+            alt=""
+          />
+        </div>
+        <div class="home-header-text">
+          <h1>welcome to me candy shop</h1>
+        </div>
       </div>
     </div>
   );
 }
 
-// https://i.pinimg.com/564x/89/78/80/897880bd2c56e94018f0a319c9365087.jpg
