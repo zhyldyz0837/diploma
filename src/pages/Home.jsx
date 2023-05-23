5410d4f4ec583358a446e54273a75549256a9f27
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-05-23 14:24:41 +0600

    Added paragraph

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,14 @@ export default function Home() {
   return (
     <div className="Home">
       <div class="home-header">
+        <div class="home-header-text">
+          <h1>Welcome to my candy shop!</h1>
+        </div>
         <div class="home-header-img">
           <img
             src="https://i.pinimg.com/564x/31/bb/15/31bb15f744904d29835a99e4946f51c1.jpg"
             alt=""
           />
-        </div>
-        <div class="home-header-text">
-          <h1>Welcome to my candy shop!</h1>
         </div>
       </div>
     </div>
