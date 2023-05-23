44ede9e586b2f4dd0467067a10761704ac9abe1f
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-05-23 14:23:41 +0600

    Changed poragraph to Contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,10 @@
 export default function Contact() {
   return (
     <div className="Contact">
-      <h1>Welcome to my Candy shop </h1>
-      <p>This is my awesome Candy shop</p>
-      <p>We only use natural ingredients to make our yummy candy shop </p>
+      <h2 className="four-address-h2">Our address:</h2>
+      <h2 className="phone-number-h2">Phone number:</h2>
+      <p className="number-1">+996 999 26 33 39</p>
+      <p className="number-2">+996 999 26 33 39</p>
     </div>
   );
 }
