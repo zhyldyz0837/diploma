c49f2bc0403b6e4a95f84f9a18fd5b1755d6a248
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-05-23 14:22:29 +0600

    on about added title and paragraph

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,13 @@
 export default function About() {
   return (
     <div className=" About">
-      <img src="https://i.pinimg.com/564x/ef/ca/3f/efca3fd2c02a6423ad2fe5ccc1e2eb9d.jpg" />
+      <div>
+        <h1> About us</h1>
+      </div>
+      <div>
+        {" "}
+        <img src="https://i.pinimg.com/564x/ef/ca/3f/efca3fd2c02a6423ad2fe5ccc1e2eb9d.jpg" />
+      </div>
     </div>
   );
 }
