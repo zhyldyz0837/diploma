a74c728fc2a4a3f30028fe67fbe63a613203565d
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-05-24 19:04:01 +0600

    changed cart icon

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,11 +2,16 @@ export default function About() {
   return (
     <div className=" About">
       <div>
-        <h1> About us</h1>
-      </div>
-      <div>
         {" "}
-        <img src="https://i.pinimg.com/564x/ef/ca/3f/efca3fd2c02a6423ad2fe5ccc1e2eb9d.jpg" />
+        <h3 className="about-h3">
+          Welcome to our online store ConfectionerShop! </h3>
+        <p className="about-p">ConfectionerShop is a
+          specialized store that has everything you need for both professional
+          confectioners and amateurs. We offer you a wide range of products for
+          confectioners: chocolate, cream, food additives, dyes, various
+          toppings, confectionery equipment and much more to create
+          confectionery masterpieces.
+        </p>
       </div>
     </div>
   );
