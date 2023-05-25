641ddf4cb452bb37fb017c022556f362fbc00e1a
bazarkulova zhydyz <zhyldyzbazarkulova497@gmail.com>
2023-05-25 15:38:17 +0600

    changed color in ause and added transition

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,8 +3,12 @@ export default function Contact() {
     <div className="Contact">
       <h2 className="four-address-h2">Our address:</h2>
       <h2 className="phone-number-h2">Phone number:</h2>
-      <p className="number-1">+996 999 26 33 39</p>
-      <p className="number-2">+996 999 26 33 39</p>
+      <a href="tel:+996999263339" className="number-1">
+        +996 999 26 33 39
+      </a> <br />
+      <a href="tel:+996999263339" className="number-2">
+        +996 999 26 33 39
+      </a>
     </div>
   );
 }
